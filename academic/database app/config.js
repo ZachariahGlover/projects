8d291e8876a34310a92fe580193869f05c02b81b
ZachariahGlover <zach.glover@live.com>
2016-05-24 20:24:20 -0400

    Adding project folders

---
+++ academic/database app/config.js
@@ -0,0 +1,10 @@
+"use strict";
+module.exports = {
+	serverhost: 'localhost', // Server address.
+	serverport: 41337, // Server port.
+	dbhost: 'localhost', // Database address.
+	dbport: 41338, // Database port.
+	dbname: '', // Database name.
+	dbuser: '', // Username for database.
+	dbpass: '' // Password for database user.
+}
